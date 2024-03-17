38acbb3544708a230757c2999ac5b10554ed89c6
Moinul-Islam-Raj <moinulislamraj0@gmail.com>
2024-03-17 23:46:09 +0600

    no socket

--- utils/sendTokenAsCookie.js
+++ utils/sendTokenAsCookie.js
@@ -7,7 +7,7 @@ module.exports = (userId, res) => {
     res.cookie('token', token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly:true,
-        sameSite:'strict',
-        secure:false,
+        sameSite:'none',
+        secure:true,
     })
 }
